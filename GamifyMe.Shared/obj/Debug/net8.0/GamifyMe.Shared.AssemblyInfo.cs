3a7e1e0b59403f168f505c5759be7cf427300ae3
Christophe CANON <christophe.canon@gmail.com>
2025-11-29 11:20:59 +0100

    pour etre sur

--- GamifyMe.Shared/obj/Debug/net8.0/GamifyMe.Shared.AssemblyInfo.cs
+++ GamifyMe.Shared/obj/Debug/net8.0/GamifyMe.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamifyMe.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ce1d61190aa99ef045443f94938465163352e35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb9609b7e8e5326dc48d574a67fea419acee0b75")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamifyMe.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamifyMe.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GamifyMe.Web/obj/Debug/net8.0/GamifyMe.Web.AssemblyInfo.cs
+++ GamifyMe.Web/obj/Debug/net8.0/GamifyMe.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamifyMe.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ce1d61190aa99ef045443f94938465163352e35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb9609b7e8e5326dc48d574a67fea419acee0b75")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamifyMe.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamifyMe.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
